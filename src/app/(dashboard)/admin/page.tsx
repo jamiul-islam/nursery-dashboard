2669202f99f26c081cdb32519d5fa1c026911b58
jamiul-islam <jamiulislamjami@hotmail.com>
2024-11-09 13:25:08 +0600

    added react-calender and events

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -1,6 +1,8 @@
 import AttendanceChart from "@/components/AttendanceChart";
 import CountChart from "@/components/CountChart";
+import EventCalendar from "@/components/EventCalendar";
 import FinanceChart from "@/components/FinanceChart";
+import Announcements from "@/components/nnouncements";
 import UserCard from "@/components/UserCard";
 
 const AdminPage = () => {
@@ -32,6 +34,10 @@ const AdminPage = () => {
         </div>
       </div>
       {/* RIGHT */}
+      <div className="w-full lg:w-1/3 flex flex-col gap-8">
+        <EventCalendar />
+        <Announcements />
+      </div>
     </div>
   );
 };
